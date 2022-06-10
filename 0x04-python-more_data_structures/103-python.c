bd6715af6886755570a53a103296e73b9849f792
cobby-obieh <cobbyobieh@gmail.com>
2022-06-09 18:34:03 -0700

    Python

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -37,4 +37,13 @@ void print_python_list(PyObject *p)
         const char *type;
 
         printf("[*] Python list info\n");
-
+        printf("[*] Size of the Python List = %li\n", size);
+        printf("[*] Allocated = %li\n", list->allocated);
+        for (i = 0; i < size; i++)
+        {
+                type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
+                if (!strcmp(type, "bytes"))
+                        print_python_bytes(list->ob_item[i]);
+        }
+}
