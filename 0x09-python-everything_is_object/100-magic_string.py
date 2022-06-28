89d72785be8f3c417314063b060d2d3843bb05e4
cobby-obieh <cobbyobieh@gmail.com>
2022-06-28 16:22:57 -0700

    Python

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-#ython3
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
+#!/usr/bin/python3
+def magic_string(i=[0]):
+    i[0] += 1
+    return str("Holberton, " * (i[0] - 1)) + "Holberton"
